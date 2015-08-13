4e80682af0eee36cf17df47e53b620e833d2f655
Phil Ruffwind <rf@rufflewind.com>
2015-08-13 02:00:32 -0400

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,11 +1,57 @@
-var map = [
-    [/mail\.google\.com/,"cs-gmail"],
-    [/twitter\.com/, "cs-twitter"]
-];
+function main() {
+    addCustomClassToHtml([
+        [/mail\.google\.com/,"cs-gmail"],
+        [/twitter\.com/, "cs-twitter"]
+    ]);
 
-var url = window.location.href;
-for (var i = 0; i < map.length; i++) {
-    if (map[i][0].test(url)) {
-        $("html").addClass(map[i][1]);
+    undisablePasswordPasting([
+    ]);
+}
+
+function addCustomClassToHtml(mapping) {
+    var url = window.location.href;
+    for (var i = 0; i < mapping.length; i++) {
+        if (mapping[i][0].test(url)) {
+            $("html").addClass(mapping[i][1]);
+        }
     }
 }
+
+function delayUntil(condition, action, interval) {
+    if (condition()) {
+        action();
+        return;
+    }
+    function callback() { delayUntil(condition, action, interval); }
+    setTimeout(callback, interval);
+}
+
+function addDelayedScript(scriptText) {
+    var script = document.createElement("script");
+    script.text = scriptText;
+    document.getElementsByTagName("html")[0].appendChild(script);
+}
+
+function stageDelayedScript(scriptText) {
+    delayUntil(
+        function() { return document.getElementsByTagName("body").length; },
+        function() { addDelayedScript(scriptText); },
+        100
+    );
+}
+
+function undisablePasswordPasting(urls) {
+    var url = window.location.href;
+    for (var i = 0; i < urls.length; i++) {
+        if (urls[i].test(url)) {
+            stageDelayedScript(
+                "Array.prototype.forEach.call(" +
+                    "document.querySelectorAll('input[type=password]')," +
+                    "function(e) { e.removeAttribute('onpaste'); }" +
+                    ");"
+            );
+        }
+    }
+}
+
+main();
