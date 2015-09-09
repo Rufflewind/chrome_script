d173e6a01ffa3405d63a9a4d468a4146a0bfed37
Phil Ruffwind <rf@rufflewind.com>
2015-09-09 02:44:33 -0400

    Minor improvements
      - Slightly improve appearance of Options UI
      - Silence errors that occurred when adding CSS to forbidden areas.
      - Move jQuery out of the repo.
      - Update jQuery and LICENSE.

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@ build:
 	mkdir -p dist
 	rm -f dist/dist.zip
 	zip dist/dist.zip LICENSE
+	cd src && curl -fsOS "https://code.jquery.com/jquery-2.1.4.min.js"
+	sha512sum -c externals.sha512sum
 	cd src && zip -r ../dist/dist.zip *
 
 clean:
--- src/background.js
+++ src/background.js
@@ -1,5 +1,9 @@
 chrome.runtime.onMessage.addListener(function(request, sender) {
     if (sender.tab && request.action == "insertCSS") {
-        chrome.tabs.insertCSS(sender.tab.id, request.details);
+        chrome.tabs.insertCSS(sender.tab.id, request.details, function() {
+            if (chrome.runtime.lastError) {
+                console.log(chrome.runtime.lastError.message);
+            }
+        });
     }
 });
