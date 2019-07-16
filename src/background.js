2b4d7a2632b94b8fb4dac0fcc54e0790b6e25f63
Phil Ruffwind <rf@rufflewind.com>
2019-07-15 22:55:59 -0700

    Allow options to be bound to a keyboard shortcut

--- src/background.js
+++ src/background.js
@@ -1,3 +1,10 @@
+chrome.commands.onCommand.addListener(function(command) {
+    switch (command) {
+        case "options":
+            chrome.runtime.openOptionsPage();
+            break;
+    }
+});
 chrome.runtime.onMessage.addListener(function(request, sender) {
     if (sender.tab && request.action == "insertCSS") {
         chrome.tabs.insertCSS(sender.tab.id, request.details, function() {
